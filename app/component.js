e42835665f29bfd347eef2e5e7b301b98bc13cad
Joey Nguyen <jnn193@gmail.com>
2015-11-19 02:24:19 -0600

    Switched from jsx-loader to babel-loader.

--- app/component.js
+++ app/component.js
@@ -4,6 +4,6 @@ module.exports = React.createClass({
     render: function() {
         return (
             <h1>Hello world!</h1>
-        )
+        );
     }
 });
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@ var path = require('path');
 // var BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 
 module.exports = {
+    // Gives you sourcemaps without slowing down rebundling
+    devtool: 'eval-source-map',
     entry: [
         'webpack/hot/dev-server',
         'webpack-dev-server/client?http://localhost:8080',
@@ -13,9 +15,16 @@ module.exports = {
     },
     module: {
         loaders: [{
-            test: /\.js$/,
+            test: /\.js?$/,
             exclude: /node_modules/,
-            loader: 'jsx-loader'
+            loader: 'babel',
+            query: {
+                // speed up babel-loader by caching
+                // transformations to the filesystem instead
+                // of default OS temporary file directory
+                cacheDirectory: true,
+                presets: ['react', 'es2015']
+            }
         }]
     }
 };
