4a2cb6063d4fced92b8c6573dfcdaba4f1cb35c5
Joey Nguyen <jnn193@gmail.com>
2015-12-18 15:47:30 -0600

    Forgot to include files from last commit.

---
+++ app.js
@@ -0,0 +1,25 @@
+import path from 'path';
+import express from 'express';
+import webpack from 'webpack';
+import webpackMiddleware from 'webpack-dev-middleware';
+import webpackHotMiddleware from 'webpack-hot-middleware';
+import config from './webpack.config.js';
+
+const app = express();
+const compiler = webpack(config);
+
+app.use(express.static(__dirname + '/build'));
+app.use(webpackMiddleware(compiler));
+app.use(webpackHotMiddleware(compiler));
+app.get('*', function response(req, res) {
+    res.sendFile(path.join(__dirname, 'build/index.html'));
+});
+
+app.listen(3000, 'localhost', function errorCheck(err) {
+    if (err) {
+        console.log(err);
+        return;
+    }
+
+    console.log('Listening at http://localhost:3000');
+});
